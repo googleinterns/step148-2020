84b10cb9d8c9d0a3443b6c3035b6b048e289789d
Diana Valles <dianapaolavalles@gmail.com>
2020-06-29 15:59:49 +0000

    Add markers to submit a report, just a textbox and a submit button

---
+++ portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
@@ -0,0 +1,84 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.sps.data.Marker;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** Handles fetching and saving markers data. */
+@WebServlet("/markers")
+public class MarkerServlet extends HttpServlet {
+    private static final Gson gson = new Gson();
+    private static final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    private static final String ENTITY_TITLE = "Marker";
+    private static final String ENTITY_PROPERTY_KEY_1 = "lat";
+    private static final String ENTITY_PROPERTY_KEY_2 = "lng";
+    private static final String ENTITY_PROPERTY_KEY_3 = "content";
+
+
+    /** Responds with JSON array containing marker data. */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        List<Marker> markers = getMarkers();
+        String json = gson.toJson(markers);
+        response.setContentType("application/json");
+        response.getWriter().println(json);
+    }
+
+    /** Accepts a POST request containing a new marker. */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lng = Double.parseDouble(request.getParameter("lng"));
+
+        /** Use to ensure that end-user provided HTML contains only elements and attributes that you 
+            are expecting; no junk */
+        try{
+            String content = Jsoup.clean(request.getParameter("content"), Whitelist.none());
+            Marker marker = new Marker(lat, lng, content);
+            storeMarker(marker);
+        }
+        catch(Error error){
+            System.out.println(error);
+        }
+    }
+
+    /** Fetches markers from Datastore. */
+    private List<Marker> getMarkers(){
+        List<Marker> markers = new ArrayList<>();
+        Query query = new Query(ENTITY_TITLE);
+        PreparedQuery results = datastore.prepare(query);
+
+        for(Entity entity: results.asIterable()){
+            double lat = (double) entity.getProperty("lat");
+            double lng = (double) entity.getProperty("lng");
+            String content = (String) entity.getProperty("content");
+
+            Marker marker = new Marker(lat, lng, content);
+            markers.add(marker);
+        }
+        return markers;
+    }
+
+    /** Stores a marker in Datastore. */
+    public void storeMarker(Marker marker){
+        Entity markerEntity = new Entity(ENTITY_TITLE);
+        markerEntity.setProperty(ENTITY_PROPERTY_KEY_1, marker.getLat());
+        markerEntity.setProperty(ENTITY_PROPERTY_KEY_2, marker.getLng());
+        markerEntity.setProperty(ENTITY_PROPERTY_KEY_3, marker.getContent());
+
+        datastore.put(markerEntity);
+    }
+}
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,9 +12,93 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+let map;
+/** Editable marker that displays when a user clicks in the map */
+let editMarker;
+
 /** Creates a map and adds it to the page. */
 function createMap(){
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 37.422, lng: -122.084 }, 
         zoom:10});
+        
+    /** When the user clicks on the map, show a marker with a form the user can edit. */ 
+    map.addListener('click', (event) => {
+        createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+    });
+
+    fetchMarkers();
+}
+
+/** Fetches markers from the backend and adds them to the map */
+function fetchMarkers(){
+    fetch('/markers').then(response => response.json()).then((markers) => {
+        markers.forEach((marker) => {
+            createMarkerForDisplay(marker.lat, marker.lng, marker.content);
+        });
+    });
+}
+
+/** Creates a marker that shows a read-only info window when clicked */
+function createMarkerForDisplay(lat, lng, content){
+    const marker = new google.maps.Marker({position: {lat: lat, lng: lng, map: map}});
+
+    var infoWindow = new google.maps.InfoWindow({content: content});
+
+    marker.addListener('click', () => {
+        infoWindow.open(map, marker);
+    });
+}
+
+/** Sends a marker to the backend for saving */
+function postMarker(lat, lng, content){
+    const params = new URLSearchParams();
+
+    params.append('lat', lat);
+    params.append('lng', lng);
+    params.append('content', content);
+
+    fetch('/markers', {method: 'POST', body: params})
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+/** Creates a marker that shows a textbox the user can edit */
+function createMarkerForEdit(lat, lng){
+    /** If we are already showing an editable marker, then remove it */
+    if(editMarker){
+        editMarker.setMap(null);
+    }
+
+    editMarker = new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+    let infoWindow = new google.maps.InfoWindow({content: buildInfoWindow(lat, lng)});
+
+    /** When the user closes the editable info window, remove the marker */
+    google.maps.event.addListener(infoWindow, 'closeclick', () => {
+        editMarker.setMap(null);
+    });
+
+    infoWindow.open(map, editMarker);
+}
+
+/** Builds and returns HTML elements that show an editable textbod and submit button */
+function buildInfoWindow(lat, lng){
+    const textbox = document.createElement('textarea');
+    const button = document.createElement('button');
+    button.appendChild(document.createTextNode('Submit'));
+
+    button.onclick = () => {
+        postMarker(lat, lng, textbox.value);
+        createMarkerForDisplay(lat, lng);
+        editMarker.setMap(null);
+    }
+
+    const containerDiv = document.createElement('div');
+    containerDiv.appendChild(textbox);
+    containerDiv.appendChild(document.createElement('br'));
+    containerDiv.appendChild(button);
+
+    return containerDiv;
 }
