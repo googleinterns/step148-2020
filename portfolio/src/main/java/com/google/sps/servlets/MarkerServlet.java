70983fc842fb32f4a705c4a499dcfa4817748e2e
Susana Perez <susanaperezjrz@gmail.com>
2020-07-10 23:48:36 +0000

    Modify servlet to only fetch markers close to the user

--- portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
@@ -5,6 +5,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.sps.data.Location;
 import com.google.sps.data.Marker;
 import com.google.gson.Gson;
 import java.io.IOException;
@@ -34,7 +35,7 @@ public class MarkerServlet extends HttpServlet {
     /** Responds with JSON array containing marker data. */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        List<Marker> markers = getMarkers();
+        List<Marker> markers = getMarkers(request);
         String json = gson.toJson(markers);
         response.setContentType("application/json");
         response.getWriter().println(json);
@@ -64,10 +65,11 @@ public class MarkerServlet extends HttpServlet {
     }
 
     /** Fetches markers from Datastore. */
-    private List<Marker> getMarkers(){
+    private List<Marker> getMarkers(HttpServletRequest request){
         List<Marker> markers = new ArrayList<>();
         Query query = new Query(ENTITY_TITLE);
         PreparedQuery results = datastore.prepare(query);
+        Location location = getUserLocation(request);
 
         for(Entity entity: results.asIterable()){
             double lat = (double) entity.getProperty("lat");
@@ -77,9 +79,11 @@ public class MarkerServlet extends HttpServlet {
             String time = (String) entity.getProperty("time");
             String address = (String) entity.getProperty("address");
             String description = (String) entity.getProperty("description");
-
-            Marker marker = new Marker(lat, lng, crime, date, time, address, description);
-            markers.add(marker);
+            //fetches markers olny if they are inside the wanted area
+            if(inArea(location, lat, lng)){ 
+                Marker marker = new Marker(lat, lng, crime, date, time, address, description);
+                markers.add(marker);
+            }
         }
         return markers;
     }
@@ -96,5 +100,26 @@ public class MarkerServlet extends HttpServlet {
         markerEntity.setProperty(ENTITY_PROPERTY_KEY_7, marker.getDescription());
 
         datastore.put(markerEntity);
+    }
+
+    public Location getUserLocation(HttpServletRequest request){
+        String locationStr = request.getParameter("location");
+        String[] locationArrStr = locationStr.split(",", 2);
+        double lat = Double.parseDouble(locationArrStr[0]);
+        double lng = Double.parseDouble(locationArrStr[1]);
+        Location userLocation = new Location(lat, lng);
+        return userLocation;
+
+    }
+
+    public boolean inArea(Location location, double markerLat, double markerLng){
+        return haversineDistance(location, markerLat, markerLng) <= 1609.34; //return markers that are within 1 mile (1609 meters) away from user
+    }
+
+    /* Returns distance between user location and a report (in meters).*/
+    public double haversineDistance(Location location, double markerLat, double markerLng) {
+        double distance = org.apache.lucene.util.SloppyMath.haversinMeters(location.getLatitude(), location.getLongitude(), markerLat, markerLng);
+        System.out.println("distance of this marker is: " + distance);
+        return distance;
     }
 }
