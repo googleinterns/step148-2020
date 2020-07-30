6b9384656eb43edeae74cd908c11519b26f46edb
Diana Valles <dianapaolavalles@gmail.com>
2020-07-30 16:11:13 +0000

    Use await and async keywords, add print statement to ensure that it works

--- portfolio/src/main/webapp/waypoints.js
+++ portfolio/src/main/webapp/waypoints.js
@@ -12,25 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function getWaypointForGrid(grid) {
-  fetch('/grids?requestRow=' + grid.row + '&requestCol=' + grid.col)
-  .then(response => response.json())
-  .then((waypoint) => {
-    let waypointForGrid = {
-      location: new google.maps.LatLng(waypoint.lat, waypoint.lng),
+var grid = new Object();
+grid.row = 1;
+grid.col = 2;
+
+async function getWaypointForGrid(grid) {
+  try{
+    let response = await fetch('/grids?requestRow=' + grid.row + '&requestCol=' + grid.col);
+    let waypoint =  response.json();
+  
+    return {
+      location: new google.maps.LatLng(waypoint.lat, waypoint.lng), 
       stopover: true
-    }
-    
-  return waypointForGrid;
-  })
-  .catch((error) => {
+    }  
+  }
+  catch(error){
     console.error(error);
-  }); 
+  }
 }
 
 /** Return the safest neighborings grid found [up, down, right, left].
     Assumption: at least one grid will be safe. */
-function getSafeNeighboringGrids(grid){
+async function getSafeNeighboringGrids(grid){
   let upperGridRow = -1;
   let lowerGridRow = -1;
   let rightGridCol = -1;
@@ -46,74 +49,80 @@ function getSafeNeighboringGrids(grid){
     upperGridRow = grid.row + 1;
 
     // Might need to change the parameters once the class for the numberOfReports is merged.
-    fetch('/numberOfReports?row=' + upperGridRow + '&col=' + grid.col)
-    .then(response => response.json())
-    .then((reportsInGrid) => {
-        if(reportsInGrid == 0){
+    try{
+      let response = await fetch('/numberOfReports?row=' + upperGridRow + '&col=' + grid.col);
+      let reportsInGrid = await response.json();
+      
+      if(reportsInGrid == 0){
         safeGridUp.row = upperGridRow;
         safeGridUp.col = grid.col;
         safeGrids[index] = safeGridUp; 
         index++;
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+      }
+    }
+    catch(error){
+      console.error(error);
+    }
   }
   
   if(grid.row - 1 > 0){
     lowerGridRow = grid.row - 1;
 
-    fetch('/numberOfReports?row=' + lowerGridRow + '&col=' + grid.col)
-    .then(response => response.json())
-    .then((reportsInGrid) => {
+    try{
+      let response = await fetch('/numberOfReports?row=' + lowerGridRow + '&col=' + grid.col);
+      let reportsInGrid = await response.json();
+
       if(reportsInGrid == 0){
         safeGridDown.row = lowerGridRow;
         safeGridDown.col = grid.col;
         safeGrids[index] = safeGridDown; 
         index++;
       }
-    })
-    .catch((error) => {
+    }
+    catch(error){
         console.error(error);
-    });
+    }
   }
 
   if(grid.col + 1 < 16){
     rightGridCol = grid.col + 1;
-
-    fetch('/numberOfReports?row=' + grid.row + '&col=' + rightGridCol)
-    .then(response => response.json())
-    .then((reportsInGrid) => {
+    
+    try{
+      let response = await fetch('/numberOfReports?row=' + grid.row + '&col=' + rightGridCol);
+      let reportsInGrid = await response.json();
+    
       if(reportsInGrid == 0){
         safeGridRight.row = grid.row;
         safeGridRight.col = rightGridCol;
         safeGrids[index] = safeGridRight; 
         index++;
       }
-    })
-    .catch((error) => {
+    }
+    catch(error){
         console.error(error);
-    });
+    }
   }
 
   if(grid.col - 1 > 0){
     leftGridCol = grid.col -1;
 
-    fetch('/numberOfReports?row=' + grid.row + '&col=' + leftGridCol)
-    .then(response => response.json())
-    .then((reportsInGrid) => {
+    try{
+      let response = await fetch('/numberOfReports?row=' + grid.row + '&col=' + leftGridCol);
+      let reportsInGrid = await response.json();
+    
       if(reportsInGrid == 0){
         safeGridLeft.row = grid.row;
         safeGridLeft.col = leftGridCol;
         safeGrids[index] = safeGridLeft;
         index++;
       }
-    })
-    .catch((error) => {
+    }
+    catch(error){
         console.error(error);
-    });
+    }
   }
 
   return safeGrids;
 }
+
+console.log(getSafeNeighboringGrids(grid)); // Remove when everything is merged.
